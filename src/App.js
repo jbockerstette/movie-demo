3deda1514e6f9d3db94c38a7191d8df63e8e6af6
jbockerstette <jim.bockerstette@gmail.com>
2018-02-13 16:16:03 -0800

    Changed name of project to movie-demo

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ class App extends Component {
     };
   }
 
-  // https://api.themoviedb.org/3/movie/550?api_key=31abd4c5ab9b3540be79a676a989bddd
-  // https://api.themoviedb.org/3/discover/movie?api_key=31abd4c5ab9b3540be79a676a989bddd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1
   componentWillMount() {
     fetch(
       'https://api.themoviedb.org/3/discover/movie?api_key=31abd4c5ab9b3540be79a676a989bddd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
